28cd03172f2488a86bd08683b1a797b7ea4acc41
Hauctopuce <15708424+HauctoPuce@users.noreply.github.com>
2020-03-02 08:22:10 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Polybar Covid Tracker
+
+## Synopsis
+
+A simple polybar module tracking coronavirus progression through the world.
---
+++ covid_tracker.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+
+# ===================================================================== #
+#  file: covid_tracker.sh                                               #
+#  Fetch coronavirus progression data from the coronavirus-tracker API  #
+#  https://github.com/ExpDev07/coronavirus-tracker-api                  #
+# ===================================================================== #
+
+echo "Hello World"
