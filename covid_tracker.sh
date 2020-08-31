5e47c35c027ce9f53a24b34bcefac0dda5eaa9a5
Laurynas Kerežius <laurynas@kerezius.com>
2020-08-31 12:59:49 +0300

    add new features

--- covid_tracker.sh
+++ covid_tracker.sh
@@ -17,5 +17,8 @@ fi
 nb_confirmed=$(jq '.["cases"]' <<< $res)
 nb_death=$(jq '.["deaths"]' <<< $res)
 nb_recovered=$(jq '.["recovered"]' <<< $res)
+nb_todayCases=$(jq '.["todayCases]' <<< $res)
+nb_todayDeaths=$(jq '.["todayDeaths]' <<< $res)
+nb_todayRecovered=$(jq '.["todayRecovered]' <<< $res)
 
-echo "  $nb_confirmed | $nb_death |  $nb_recovered "
+echo "  $nb_confirmed($nb_todayCases) | $nb_death($nb_todayDeaths) |  $nb_recovered($nb_todayRecovered) "
